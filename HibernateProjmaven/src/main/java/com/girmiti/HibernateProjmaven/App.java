f52f533312856c661915a4ff881aeefc0768eba9
vijaykumar <vijay.bandaragalla@GSPLK-CPU0249.girmiti.in>
2023-03-30 17:17:12 +0530

    first commit of hibernate with the help of maven dependencies

---
+++ HibernateProjmaven/src/main/java/com/girmiti/HibernateProjmaven/App.java
@@ -0,0 +1,14 @@
+package com.girmiti.HibernateProjmaven;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "project started" );
+    }
+}
